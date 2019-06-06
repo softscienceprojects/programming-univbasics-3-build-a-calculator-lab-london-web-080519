2b949405e776fbaafa2cf1e134beeaeebe034b1b
erin johnson <erin.nicole.johnson@gmail.com>
2019-06-06 20:50:57 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  sum
 end
 
 def subtraction(num1, num2)
-
+  subs = num1 - num2
+  subs
 end
 
 def division(num1, num2)
-
+  divs = num1 / num2
+  divs
 end
 
 def multiplication(num1, num2)
-
+  mults = num1 * num2
+  mults
 end
 
 def modulo(num1, num2)
-
+  mods = num1 % num2
+  mods
 end
 
 def square_root(num)
-
+  sqs = Math.sqrt(num)
+  sqs
 end
